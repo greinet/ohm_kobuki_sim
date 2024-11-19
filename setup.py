01c5856a362ab509981f80d684436e9baaee91ce
Marco Masannek <masanator@hotmail.de>
2024-11-19 21:18:39 +0100

    fixing setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     version='0.0.0',
-    scripts=['scripts/ohm_mecanum_sim_node.py'],
+    scripts=['scripts/ohm_kobuki_sim_node.py'],
     packages=['ohm_kobuki_sim'],
     package_dir={'': 'src'}
 )
